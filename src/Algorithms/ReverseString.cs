39477d35dd596ed0093974ac66a3d7656a5a8a17
HenriqueSD <hsd.foz@gmail.com>
2019-12-08 19:18:18 -0300

    String Reversal

---
+++ src/Algorithms/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    public static class ReverseString
+    {
+        public static string Reverse(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        public static string ReverseUsingLinq(string str)
+        {
+            return new string (str.Reverse().ToArray());
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/ReverseStringTest.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class ReverseStringTest
+    {
+        [Theory]
+        [InlineData("apple", "elppa")]
+        [InlineData("hello", "olleh")]
+        [InlineData("  abc", "cba  ")]
+        public void Reverse_ShouldReverseString(string str, string strExpected)
+        {
+            var result = ReverseString.Reverse(str);
+
+            Assert.Equal(result, strExpected);
+        }
+
+        [Theory]
+        [InlineData("apple", "elppa")]
+        [InlineData("hello", "olleh")]
+        [InlineData("  abc", "cba  ")]
+        public void ReverseUsingLinq_ShouldReverseString(string str, string strExpected)
+        {
+            var result = ReverseString.ReverseUsingLinq(str);
+
+            Assert.Equal(result, strExpected);
+        }
+    }
+}
