04b034a658f31ca5f45b0583fe5ce062656d1c97
HenriqueSD <hsd.foz@gmail.com>
2019-12-09 20:02:47 -0300

    Integer Reversal

---
+++ src/Algorithms/ReverseInt.cs
@@ -0,0 +1,17 @@
+﻿namespace Algorithms
+{
+    public static class ReverseInt
+    {
+        public static int Reverse(int number)
+        {
+            int reverse = 0, rem;
+            while (number != 0)
+            {
+                rem = number % 10;
+                reverse = reverse * 10 + rem;
+                number /= 10;
+            }
+            return reverse;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/ReverseIntTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class ReverseIntTest
+    {
+        [Theory]
+        [InlineData(15, 51)]
+        [InlineData(981, 189)]
+        [InlineData(500, 5)]
+        [InlineData(-15, -51)]
+        [InlineData(-90, -9)]
+        public void Reverse_ShouldReverseInt(int number, int expectedNumber)
+        {
+            var result = ReverseInt.Reverse(number);
+
+            Assert.Equal(result, expectedNumber);
+        }
+    }
+}
