71c863db1ec1f7daeb37307ef41bc8f47743586b
henriquesd <hsd.foz@gmail.com>
2019-12-09 17:07:43 -0300

    Palindromes

---
+++ src/Algorithms/Palindromes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    public static class Palindromes
+    {
+        public static bool IsPalindrome(string str)
+        {
+            string reversed;
+            char[] ch = str.ToCharArray();
+
+            Array.Reverse(ch);
+            reversed = new string(ch);
+
+            return str.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool IsPalindromeUsingLinq(string str)
+        {
+            return str.SequenceEqual(str.Reverse());
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/PalindromesTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class PalindromesTest
+    {
+        [Theory]
+        [InlineData("abba", true)]
+        [InlineData("abcdefg", false)]
+        public void IsPalindrome_ShouldReturnTrueOrFalse_IfWordIsPalindrome(string str, bool expectedValue)
+        {
+            var result = Palindromes.IsPalindrome(str);
+
+            Assert.Equal(result, expectedValue);
+        }
+
+        [Theory]
+        [InlineData("abba", true)]
+        [InlineData("abcdefg", false)]
+        public void IsPalindromeUsingLinq_ShouldReturnTrueOrFalse_IfWordIsPalindrome(string str, bool expectedValue)
+        {
+            var result = Palindromes.IsPalindromeUsingLinq(str);
+
+            Assert.Equal(result, expectedValue);
+        }
+    }
+}
