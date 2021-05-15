b66a7516e00adf76920632620d0b026e667b6149
henriquesd <hsd.foz@gmail.com>
2021-05-15 19:19:01 +0200

    Anagram

---
+++ src/Algorithms/Anagram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class Anagram
+    {
+        public static bool AreAnagram(string text1, string text2)
+        {
+            if (text1.ToLower().Equals(text2.ToLower())) return true;
+
+            var array1 = text1.Replace(" ", "").ToLower().ToCharArray();
+            var array2 = text2.Replace(" ", "").ToLower().ToCharArray();
+
+            Array.Sort(array1);
+            Array.Sort(array2);
+
+            if (array1.Length != array2.Length) return false;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
