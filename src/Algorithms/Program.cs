588a4ebcd2eee28a5a21bbffdc3f4cc6a32ca5db
henriquesd <hsd.foz@gmail.com>
2022-07-05 20:14:30 +0200

    Updated project to .NET 6

--- src/Algorithms/Program.cs
+++ src/Algorithms/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace Algorithms
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.ReadLine();
-        }
-    }
-}
+﻿Console.ReadLine();
