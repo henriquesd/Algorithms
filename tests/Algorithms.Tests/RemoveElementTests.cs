94a2a5344a86f2c2a30be678798b1bf5bd254407
henriquesd <hsd.foz@gmail.com>
2023-09-04 21:49:01 +0200

    Added more tests for Remove Element

--- tests/Algorithms.Tests/RemoveElementTests.cs
+++ tests/Algorithms.Tests/RemoveElementTests.cs
@@ -7,6 +7,9 @@ namespace Algorithms.Tests
         [Theory]
         [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5)]
         [InlineData(new int[] { 3, 2, 2, 3 }, 3, 2)]
+        [InlineData(new int[] { 4, 4, 2, 4, 5, 1, 2, 3, 1 }, 4, 6)]
+        [InlineData(new int[] { 2, 0, 1 }, 1, 2)]
+        [InlineData(new int[] { 6, 2, 0, 1, 3, 5 }, 2, 5)]
         [InlineData(new int[] { }, 1, 0)]
         [InlineData(null, 1, 0)]
         public void Remove_ShouldReturnCorrectValues(int[] inputArray, int inputValue, int expectedResult)
