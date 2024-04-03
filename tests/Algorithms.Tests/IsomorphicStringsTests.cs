bf9559317721e57f3300a3eb4610df622bbe4e62
henriquesd <hsd.foz@gmail.com>
2024-04-03 21:01:55 +0200

    IsomorphicStrings

---
+++ tests/Algorithms.Tests/IsomorphicStringsTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class IsomorphicStringsTests
+    {
+        [Theory]
+        [InlineData("egg", "add", true)]
+        [InlineData("foo", "bar", false)]
+        [InlineData("paper", "title", true)]
+        public void IsIsomorphic_ShouldReturnsExpectedValue(string s, string t, bool expectedValue)
+        {
+            var result = IsomorphicStrings.IsIsomorphic(s, t);
+
+            Assert.Equivalent(expectedValue, result);
+        }
+    }
+}
