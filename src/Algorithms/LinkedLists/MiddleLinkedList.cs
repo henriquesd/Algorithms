ec9377d7bc1de1c2ca43382a33e1f4d0c5117b7d
henriquesd <hsd.foz@gmail.com>
2024-04-03 18:37:53 +0200

    MiddleLinkedList

---
+++ src/Algorithms/LinkedLists/MiddleLinkedList.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.LinkedLists
+{
+    /// <summary>
+    /// Given the head of a singly linked list, return the middle node of the linked list.
+    /// If there are two middle nodes, return the second middle node.
+    /// </summary>
+    public static class MiddleLinkedList
+    {
+        public static ListNode MiddleNode(ListNode head)
+        {
+            ListNode middle = head;
+            ListNode end = head;
+
+            while (end != null && end.next != null)
+            {
+                middle = middle.next;
+                end = end.next.next;
+            }
+
+            return middle;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/LinkedLists/MiddleLinkedListTests.cs
@@ -0,0 +1,41 @@
+﻿using Algorithms.LinkedLists;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests.LinkedLists
+{
+    public class MiddleLinkedListTests
+    {
+        [Theory]
+        [MemberData(nameof(ValuesToTest))]
+        public void MiddleNode_ShouldReturnsMiddleNode(ListNode listNode, ListNode middleOfLinkedList)
+        {
+            var result = MiddleLinkedList.MiddleNode(listNode);
+
+            Assert.Equivalent(middleOfLinkedList, result);
+        }
+
+        public static IEnumerable<object[]> ValuesToTest()
+        {
+            yield return new object[] { CreateLinkedList(new int[] { 1, 2, 3, 4, 5, 6 }), CreateLinkedList(new int[] { 4, 5, 6 }) };
+            yield return new object[] { CreateLinkedList(new int[] { 1, 2, 3, 4, 5 }), CreateLinkedList(new int[] { 3, 4, 5 }) };
+        }
+
+        private static ListNode CreateLinkedList(int[] values)
+        {
+            if (values == null || values.Length == 0)
+                return null;
+
+            ListNode head = new ListNode(values[0]);
+            ListNode current = head;
+
+            for (int i = 1; i < values.Length; i++)
+            {
+                current.next = new ListNode(values[i]);
+                current = current.next;
+            }
+
+            return head;
+        }
+    }
+}
