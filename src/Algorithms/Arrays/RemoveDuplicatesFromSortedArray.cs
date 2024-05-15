e0ad4d0b67a79b2e44e38efb82494e0be27f9614
henriquesd <hsd.foz@gmail.com>
2024-05-15 20:42:05 +0200

    RemoveDuplicatesFromSortedArray

---
+++ src/Algorithms/Arrays/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.Arrays
+{
+    public static class RemoveDuplicatesFromSortedArray
+    {
+        // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that
+        //   each unique element appears only once.
+        // The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+        // Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+        //  1. Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+        //    The remaining elements of nums are not important as well as the size of nums.
+        //  2. Return k.
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            var insertIndex = 1;
+
+            // Iterate through the array starting from the second element;
+            for (int i = 1; i < nums.Length; i++)
+            { 
+                // Compare if the previous element is different than the current element;
+                if (nums[i - 1] != nums[i])
+                {
+                    // If they are different, move the element to the insertIndex position;
+                    nums[insertIndex] = nums[i];
+
+                    insertIndex++;
+                }
+            }
+
+            // The value of insertIndex represents the number of unique elements
+            // and also serves as the length of the resulting array;
+            return insertIndex;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Arrays/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Arrays;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [Theory]
+        [MemberData(nameof(ValuesToTest))]
+        public void RemoveDuplicatesFromSortedArrayTests_ShouldRemoveDuplicates(int[] nums, int expectedResult)
+        {
+            var result = RemoveDuplicatesFromSortedArray.RemoveDuplicates(nums);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> ValuesToTest()
+        {
+            yield return new object[] { new int[] { 1, 1, 2 }, 2};
+            yield return new object[] { new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5 };
+            yield return new object[] { new int[] { }, 0 }; // Empty array
+            yield return new object[] { new int[] { 1 }, 1 }; // Single element array
+            yield return new object[] { new int[] { 1, 2, 3 }, 3 }; // No duplicates
+            yield return new object[] { new int[] { 1, 1, 1, 1 }, 1 }; // All elements are duplicates
+            yield return new object[] { new int[] { 1, 1, 2, 2, 3, 3, 4, 4 }, 4 }; // Alternating duplicates
+        }
+    }
+}
