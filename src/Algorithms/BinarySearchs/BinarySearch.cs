c206a736252c2c6e023bf962d19042c5232c5a40
henriquesd <hsd.foz@gmail.com>
2024-05-16 20:39:44 +0200

    Updated BinarySearch

--- src/Algorithms/BinarySearchs/BinarySearch.cs
+++ src/Algorithms/BinarySearchs/BinarySearch.cs
@@ -2,30 +2,34 @@
 {
     public static class BinarySearch
     {
-        public static int? FindNumberPositionInSortedArray(int[] inputArray, int searchedNumber)
+        // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+        // If target exists, then return its index. Otherwise, return -1.
+        // You must write an algorithm with O(log n) runtime complexity.
+        public static int Search(int[] nums, int target)
         {
-            int min = 0;
-            int max = inputArray.Length - 1;
+            // Initialize the left and right pointers for the search range;
+            var left = 0;
+            var right = nums.Length - 1;
 
-            while (min <= max)
+            // Perform binary search as long as the search range is valid;
+            while (left <= right)
             {
-                int mid = (min + max) / 2;
+                // Calculate the middle index of the current search range;
+                // We continuously divide the search range in half until the target element is found or the search range becomes empty;
+                var middle = left + (right - left) / 2;
 
-                if (searchedNumber == inputArray[mid])
-                {
-                    return mid;
-                }
-                else if (searchedNumber < inputArray[mid])
-                {
-                    max = mid - 1;
-                }
-                else
-                {
-                    min = mid + 1;
-                }
+                // If the middle element is the target, return its index;
+                if (nums[middle] == target) return middle;
+
+                // If the target is greater than the middle element, search the right half
+                if (nums[middle] < target) left = middle + 1;
+
+                // If the target is smaller than the middle element, search the left half
+                if (nums[middle] > target) right = middle - 1;
             }
 
-            return null;
+            // If the target is not found, return -1;
+            return -1;
         }
 
         // It's also possible to use the method Array.BinarySearch(Array array, object? value)
--- tests/Algorithms.Tests/BinarySearchs/BinarySearchTests.cs
+++ tests/Algorithms.Tests/BinarySearchs/BinarySearchTests.cs
@@ -1,4 +1,5 @@
 ﻿using Algorithms.BinarySearchs;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Algorithms.Tests.BinarySearchs
@@ -6,16 +7,21 @@ namespace Algorithms.Tests.BinarySearchs
     public class BinarySearchTests
     {
         [Theory]
-        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 1, 0)]
-        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9, 8)]
-        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 15, null)]
-        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4, 3)]
-        [InlineData(new int[] { 4, 110, 180, 250, 340, 480, 590, 1050 }, 480, 5)]
-        public void FindNumberPositionInSortedArray_ShouldReturnSearchedNumberPosition(int[] inputArray, int searchedNumber, int? expectedPosition)
+        [MemberData(nameof(ValuesToTest))]
+        public void Search_ShouldReturnSearchedNumberPosition(int[] inputArray, int searchedNumber, int? expectedPosition)
         {
-            var result = BinarySearch.FindNumberPositionInSortedArray(inputArray, searchedNumber);
+            var result = BinarySearch.Search(inputArray, searchedNumber);
 
             Assert.Equal(expectedPosition, result);
+        }
+
+        public static IEnumerable<object[]> ValuesToTest()
+        {
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 1, 0 }; // First element
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9, 8 }; // Last element
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 15, -1 }; // Element not found
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4, 3 }; // Middle element
+            yield return new object[] { new int[] { -10, -5, 0, 5, 10 }, -10, 0 }; // Negative numbers
         }
     }
 }
