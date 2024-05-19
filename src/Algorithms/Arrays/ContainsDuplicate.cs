fa5702663ed835ae066f45635b54c621b047c52b
henriquesd <hsd.foz@gmail.com>
2024-05-19 19:36:53 +0200

    ContainsDuplicate

---
+++ src/Algorithms/Arrays/ContainsDuplicate.cs
@@ -0,0 +1,37 @@
+﻿namespace Algorithms.Arrays
+{
+    // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+    public static class ContainsDuplicate
+    {
+        public static bool ContainsDuplicateExample1(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool ContainsDuplicateExample2(int[] nums)
+        {
+            // Initialize a HashSet to store unique elements from the array;
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (!seen.Add(num))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Arrays/ContainsDuplicateTests.cs
@@ -0,0 +1,38 @@
+﻿using Algorithms.Arrays;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class ContainsDuplicateTests
+    {
+        [Theory]
+        [MemberData(nameof(ValuesToTest))]
+        public void ContainsDuplicateExample1_ShouldReturnExpectedResult(int[] inputArray, bool expectedResult)
+        {
+            var result = ContainsDuplicate.ContainsDuplicateExample1(inputArray);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValuesToTest))]
+        public void ContainsDuplicateExample2_ShouldReturnExpectedResult(int[] inputArray, bool expectedResult)
+        {
+            var result = ContainsDuplicate.ContainsDuplicateExample2(inputArray);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> ValuesToTest()
+        {
+            yield return new object[] { new int[] { 1, 2, 3, 1 }, true }; // Duplicate element exists
+            yield return new object[] { new int[] { 1, 2, 3, 4 }, false }; // All elements are unique
+            yield return new object[] { new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true }; // Multiple duplicates
+            yield return new object[] { new int[] { 1 }, false }; // Single element array
+            yield return new object[] { new int[] { }, false }; // Empty array
+            yield return new object[] { new int[] { 2, 2, 2, 2, 2 }, true }; // Array with all elements the same
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, false }; // Array with no duplicates
+        }
+    }
+}
