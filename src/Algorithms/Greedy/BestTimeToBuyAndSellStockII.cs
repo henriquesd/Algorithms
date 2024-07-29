2f185fe11c44381ea3778aec66aaae56c0e8edf5
henriquesd <hsd.foz@gmail.com>
2024-07-29 17:57:28 +0200

    BestTimeToBuyAndSellStockII

---
+++ src/Algorithms/Greedy/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithms.Greedy
+{
+    // You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+    // On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However,
+    //  you can buy it then immediately sell it on the same day.
+    // Find and return the maximum profit you can achieve.
+    public static class BestTimeToBuyAndSellStockII
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                // If the price of the stock on day i is greater than the price on day i-1, 
+                // then we can achieve a profit by buying on day i-1 and selling on day i.
+                if (prices[i] > prices[i - 1])
+                {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Greedy/BestTimeToBuyAndSellStockIITests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Greedy;
+using Xunit;
+
+namespace Algorithms.Tests.Greedy
+{
+    public class BestTimeToBuyAndSellStockIITests
+    {
+        [Theory]
+        [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 7)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4)]
+        [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
+        [InlineData(new int[] { 5 }, 0)]
+        [InlineData(new int[] { 1, 1, 1, 1, 1 }, 0)]
+        [InlineData(new int[] { 3, 3, 5, 0, 0, 3, 1, 4 }, 8)]
+
+        public void MaxProfit_ShouldReturnMaxProfit(int[] inputArray, int expectedProfit)
+        {
+            var result = BestTimeToBuyAndSellStockII.MaxProfit(inputArray);
+
+            Assert.Equal(expectedProfit, result);
+        }
+    }
+}
