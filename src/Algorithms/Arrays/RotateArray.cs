d7bf986f463e97f0daf15d5329987399083b9b33
henriquesd <hsd.foz@gmail.com>
2024-07-29 17:50:17 +0200

    RotateArray

---
+++ src/Algorithms/Arrays/RotateArray.cs
@@ -0,0 +1,48 @@
+﻿namespace Algorithms.Arrays
+{
+    // Given an integer array nums, rotate the array to the right by k steps, where k is non-negative;
+    public static class RotateArray
+    {
+        public static int[] Rotate(int[] nums, int k)
+        {
+            int numsLength = nums.Length;
+
+            // Ensure that if "k" is greater than the length of the array, 
+            // it will not rotate the array more than necessary;
+            k = k % numsLength;
+
+            // Reverse the entire array;
+            Reverse(nums, 0, numsLength - 1);
+
+            // Reverse the first k elements;
+            Reverse(nums, 0, k - 1);
+
+            // Reverse the remaining n - k elements;
+            Reverse(nums, k, numsLength - 1);
+
+            return nums;
+        }
+
+        public static void Reverse(int[] nums, int start, int end)
+        {
+            // Swap elements from the start and end of the array until they meet in the middle;
+            while (start < end)
+            {
+                // Temporarily store the value at the start index;
+                int temp = nums[start];
+
+                // Assign the value at the end index to the start index;
+                nums[start] = nums[end];
+
+                // Assign the temporarily stored value to the end index;
+                nums[end] = temp;
+
+                // Move the start index forward
+                start++;
+
+                // Move the end index backward
+                end--;
+            }
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Arrays/RotateArrayTests.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Arrays;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class RotateArrayTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3, new int[] { 5, 6, 7, 1, 2, 3, 4 })]
+        [InlineData(new int[] { -1, -100, 3, 99 }, 2, new int[] { 3, 99, -1, -100 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 10, new int[] { 5, 6, 7, 1, 2, 3, 4 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 0, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        [InlineData(new int[] { 1, 2, 3 }, 0, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { 1 }, 1, new int[] { 1 })]
+        public void Rotate_ShouldRotateArray(int[] inputArray, int rotate, int[] expectedResult)
+        {
+            var result = RotateArray.Rotate(inputArray, rotate);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
