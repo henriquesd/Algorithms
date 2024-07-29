52b62095ecb7b2cab8231f5fa25da6a5df03c02a
henriquesd <hsd.foz@gmail.com>
2024-07-29 19:46:13 +0200

    IntersectionTwoArraysII

---
+++ src/Algorithms/Arrays/IntersectionTwoArraysII.cs
@@ -0,0 +1,45 @@
+﻿namespace Algorithms.Arrays
+{
+    // Given two integer arrays nums1 and nums2, return an array of their intersection.
+    // Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order;
+    public static class IntersectionTwoArraysII
+    {
+        public static int[] Intersect(int[] nums1, int[] nums2)
+        {
+            // Dictionary to store the count of each element in nums1;
+            var counts = new Dictionary<int, int>();
+
+            // Count each element in nums1 and store in the dictionary;
+            foreach (int num in nums1)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    // Increment the count if the element is already in the dictionary;
+                    counts[num]++;
+                }
+                else
+                {
+                    // Add the element with count 1 if it is not in the dictionary
+                    counts[num] = 1;
+                }
+            }
+
+            // List to store the intersection result;
+            var result = new List<int>();
+
+            // Iterate through nums2 and add common elements to the result list;
+            foreach (int num in nums2)
+            {
+                if (counts.ContainsKey(num) && counts[num] > 0)
+                {
+                    result.Add(num); // Add the element to the result list;
+                    counts[num]--; // Decrement the count for the added element;
+                }
+            }
+
+            var resultArray = result.ToArray();
+
+            return resultArray;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Arrays/IntersectionTwoArraysIITests.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms.Arrays;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class IntersectionTwoArraysIITests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 }, new int[] { 2, 2 })]
+        [InlineData(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }, new int[] { 9, 4 })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 })]
+        public void Intersect_ShouldReturnIntersection(int[] nums1, int[] nums2, int[] expectedResult)
+        {
+            var result = IntersectionTwoArraysII.Intersect(nums1, nums2);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
