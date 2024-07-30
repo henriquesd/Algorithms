e940dc8bf73960642705d859f942c22f20a9ebfa
henriquesd <hsd.foz@gmail.com>
2024-07-30 17:48:09 +0200

    PlusOne

---
+++ src/Algorithms/Arrays/PlusOne.cs
@@ -0,0 +1,37 @@
+﻿namespace Algorithms.Arrays
+{
+    // You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+    // The digits are ordered from most significant to least significant in left-to-right order.
+    // The large integer does not contain any leading 0's.
+    // Increment the large integer by one and return the resulting array of digits;
+    public static class PlusOne
+    {
+        public static int[] Execute(int[] digits)
+        {
+            // Iterate through the array from the end (least significant digit) to the beginning (most significant digit);
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                // If the current digit is less than 9, increment it by one;
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+
+                    // Return the array immediately since there's no carry-over needed;
+                    return digits;
+                }
+
+                // If the current digit is 9, it becomes 0 and we need to carry over 1 to the next significant digit;
+                digits[i] = 0;
+            }
+
+            // If all the digits were 9, we need to handle the overflow by adding an extra digit;
+            // Create a new array with one additional length, all elements initialized to 0;
+            int[] result = new int[digits.Length + 1];
+
+            // Set the most significant digit to 1 (e.g., 999 + 1 = 1000);
+            result[0] = 1;
+
+            return result;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Arrays/PlusOneTests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Arrays;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class PlusOneTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1 }, new int[] { 2 })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
+        [InlineData(new int[] { 4, 3, 2, 1 }, new int[] { 4, 3, 2, 2 })]
+        [InlineData(new int[] { 1, 2, 9 }, new int[] { 1, 3, 0 })]
+        [InlineData(new int[] { 9, 9, 9 }, new int[] { 1, 0, 0, 0 })]
+        [InlineData(new int[] { 0,0,1 }, new int[] { 0,0,2 })]
+        [InlineData(new int[] {  }, new int[] { 1 })]
+        public void PlusOne_ShouldReturnExpectedValue(int[] inputArray, int[] expectedResult)
+        {
+            var result = PlusOne.Execute(inputArray);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
