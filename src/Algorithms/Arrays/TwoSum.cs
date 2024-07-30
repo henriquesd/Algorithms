d07b3402dd70a38207675a68b07a0a755f492cda
henriquesd <hsd.foz@gmail.com>
2024-07-30 20:12:27 +0200

    TwoSum

---
+++ src/Algorithms/Arrays/TwoSum.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.Arrays
+{
+    // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    // You can return the answer in any order;
+    public static class TwoSum
+    {
+        public static int[]? Execute(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // Calculate the complement of the current number by subtracting it from the target;
+                int complement = target - nums[i];
+
+                // Check if the complement is already in the dictionary;
+                if (map.ContainsKey(complement))
+                {
+                    // Return the indices of the two numbers that add up to the target;
+                    return new int[] { map[complement], i };
+                }
+
+
+                // Add the current number and its index to the dictionary;
+                map[nums[i]] = i;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Arrays/TwoSumTests.cs
@@ -0,0 +1,21 @@
+﻿using Algorithms.Arrays;
+using Xunit;
+
+namespace Algorithms.Tests.Arrays
+{
+    public class TwoSumTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        [InlineData(new int[] { -3, 4, 3, 90 }, 0, new int[] { 0, 2 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 10, null)]
+        public void TwoSumTests_ShouldReturnExpectedValue(int[] inputArray, int target, int[] expectedResult)
+        {
+            var result = TwoSum.Execute(inputArray, target);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
