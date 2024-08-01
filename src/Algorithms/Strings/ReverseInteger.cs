9624ff799f68e94a6023430aa038d541db78e6e7
henriquesd <hsd.foz@gmail.com>
2024-08-01 20:59:06 +0200

    ReverseInteger

---
+++ src/Algorithms/Strings/ReverseInteger.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Strings
+{
+    public static class ReverseInteger
+    {
+        public static int Reverse(int x)
+        {
+            int reversed = 0;
+
+            while (x != 0)
+            {
+                // Extract the last digit of x;
+                int pop = x % 10;
+
+                // "x" is divided by "10", to remove the last digit from x;
+                x /= 10;
+
+                // Check for overflow before actually reversing;
+                if (reversed > int.MaxValue / 10 || (reversed == int.MaxValue / 10 && pop > 7)) // int.MaxValue is 2147483647, and the last digit 7 must be taken into account;
+                {
+                    return 0;
+                }
+                if (reversed < int.MinValue / 10 || (reversed == int.MinValue / 10 && pop < -8)) // int.MinValue is -2147483648, and the last digit -8 must be taken into account;
+                {
+                    return 0;
+                }
+
+                // Push the digit to the reversed number;
+                reversed = reversed * 10 + pop;
+            }
+
+            return reversed;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Strings/ReverseIntegerTests.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Strings;
+using Xunit;
+
+namespace Algorithms.Tests.Strings
+{
+    public class ReverseIntegerTests
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        [InlineData(0, 0)]
+        [InlineData(1534236469, 0)] // Exceeds 32-bit signed integer range when reversed
+        [InlineData(-2147483648, 0)] // Exceeds 32-bit signed integer range when reversed
+        public void Reverse_ShouldReturnReversedInteger(int input, int expected)
+        {
+            var result = ReverseInteger.Reverse(input);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
