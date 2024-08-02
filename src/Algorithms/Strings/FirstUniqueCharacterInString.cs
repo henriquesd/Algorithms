b9618adbc728933d4277d1648a78800ee878ece9
henriquesd <hsd.foz@gmail.com>
2024-08-02 18:06:22 +0200

    FirstUniqueCharacterInString

---
+++ src/Algorithms/Strings/FirstUniqueCharacterInString.cs
@@ -0,0 +1,39 @@
+﻿namespace Algorithms.Strings
+{
+    public static class FirstUniqueCharacterInString
+    {
+        // Given a string s, find the first non-repeating character in it and return its index.
+        // If it does not exist, return -1;
+        public static int FirstUniqChar(string s)
+        {
+            // Dictionary to store the frequency of each character;
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+;
+            // Iterate over the string to count the frequency of each character;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+            // Iterate over the string again to find the first non-repeating character;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount[s[i]] == 1)
+                {
+                    return i; // Return the index of the first non-repeating character;
+                }
+            }
+
+            // If no non-repeating character is found, return -1;
+            return -1;
+        }
+    }
+}
---
+++ tests/Algorithms.Tests/Strings/FirstUniqueCharacterInStringTests.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Strings;
+using Xunit;
+
+namespace Algorithms.Tests.Strings
+{
+    public class FirstUniqueCharacterInStringTests
+    {
+        [Theory]
+        [InlineData("code", 0)] // Test case where the first non-repeating character is at index 0;
+        [InlineData("lovealgorithms", 2)] // Test case where the first non-repeating character is at index 2;
+        [InlineData("aabb", -1)] // Test case where there are no non-repeating characters;
+        [InlineData("z", 0)] // Test case with a single character;
+        [InlineData("abc", 0)]  // Test case with all characters unique;
+        [InlineData("aabbccd", 6)]  // Test case with non-repeating character at the end;
+        public void FirstUniqueCharacterInString_ShouldReturnFirstUniqueCharacter(string input, int expected)
+        {
+            var result = FirstUniqueCharacterInString.FirstUniqChar(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
